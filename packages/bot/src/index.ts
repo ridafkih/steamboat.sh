d7d4e751ebe4b7859e9d6cea85c32fe51bb31704
ridafkih <hello@rida.dev>
2026-01-01 12:19:10 -0700

    chore(bot): kickoff

---
+++ packages/bot/src/index.ts
@@ -0,0 +1,12 @@
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import { db } from "@steam-eye/database";
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
