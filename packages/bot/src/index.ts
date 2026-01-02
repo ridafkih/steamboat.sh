2c6fd82556973ce555d2303c6a2cdea9342896d3
ridafkih <hello@rida.dev>
2026-01-01 21:59:58 -0700

    validate length of discord token at least >0

--- packages/bot/src/index.ts
+++ packages/bot/src/index.ts
@@ -3,7 +3,7 @@ import { createClient } from "@steam-eye/api/client";
 import { entry } from "@steam-eye/entry-point";
 
 entry("bot")
-  .env({ DISCORD_TOKEN: "string", API_URL: "string.url" })
+  .env({ DISCORD_TOKEN: "string>0", API_URL: "string.url" })
   .setup(({ env }) => {
     const api = createClient(env.API_URL);
     return { discordToken: env.DISCORD_TOKEN, api };
