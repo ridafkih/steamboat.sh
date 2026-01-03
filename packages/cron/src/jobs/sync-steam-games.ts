bcfdb87059a928b2f3a0eeb0651eb88ce57b9049
ridafkih <hello@rida.dev>
2026-01-02 17:17:04 -0700

    docs(web, cron): update .env.template

--- packages/cron/src/jobs/sync-steam-games.ts
+++ packages/cron/src/jobs/sync-steam-games.ts
@@ -3,6 +3,7 @@ import type { CronContext, CronJobDefinition } from "../types";
 export default ({ api }: CronContext): CronJobDefinition => ({
   name: "sync-steam-games",
   cron: "@every_5_minutes",
+  delay: "5s",
   immediate: process.env.NODE_ENV !== "production",
   callback: async (context) => {
     const result = await api.admin.steam.syncAll();
